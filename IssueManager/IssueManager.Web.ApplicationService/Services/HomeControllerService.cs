07c6802d9c49bcb5f87977f2918f6f6402d6d8cd
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-10 17:26:04 +0900

    ホームControllerサービスを追加。

---
+++ IssueManager/IssueManager.Web.ApplicationService/Services/HomeControllerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IssueManager.Web.ApplicationService.ViewModels.Home;
+
+namespace IssueManager.Web.ApplicationService.Services
+{
+    /// <summary>
+    /// ホームControllerサービス
+    /// </summary>
+    public class HomeControllerService : IHomeControllerService
+    {
+        /// <summary>
+        /// ログインする。
+        /// </summary>
+        /// <param name="viewModel">ログインViewModel</param>
+        /// <returns>ログイン可否</returns>
+        public bool Login(LoginViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IssueManager/IssueManager.Web.ApplicationService/Services/IHomeControllerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IssueManager.Web.ApplicationService.ViewModels.Home;
+
+namespace IssueManager.Web.ApplicationService.Services
+{
+    /// <summary>
+    /// ホームControllerサービス
+    /// </summary>
+    public interface IHomeControllerService
+    {
+        /// <summary>
+        /// ログインする。
+        /// </summary>
+        /// <param name="viewModel">ログインViewModel</param>
+        /// <returns>ログイン可否</returns>
+        bool Login(LoginViewModel viewModel);
+    }
+}
