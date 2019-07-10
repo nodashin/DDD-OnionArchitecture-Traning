2f64734714e4aad7a9fb8883df54dae47e4bb624
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-10 15:25:53 +0900

    ログインViewModel追加。

---
+++ IssueManager/IssueManager.Web.ApplicationService/ViewModels/Home/LoginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueManager.Web.ApplicationService.ViewModels.Home
+{
+    /// <summary>
+    /// ログインViewModel
+    /// </summary>
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// ユーザーID
+        /// </summary>
+        [DisplayName("ユーザーID")]
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// パスワード
+        /// </summary>
+        [DisplayName("パスワード")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// ログイン後のリダイレクトURL
+        /// </summary>
+        public string ReturnUrl { get; set; }
+    }
+}
