606e74b12f2d28566058e90aa1f9489b2cc54536
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-11 13:04:23 +0900

    最低限のドメインを追加。

---
+++ IssueManagementSystem/IssueManagementSystem.DomainModel/Auth/LoginUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueManagementSystem.DomainModel.Auth
+{
+    /// <summary>
+    /// ログインユーザー
+    /// </summary>
+    public class LoginUser
+    {
+        #region プロパティ
+        /// <summary>
+        /// ユーザーID
+        /// </summary>
+        public string UserId { get => User.UserId; }
+
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        public string UserName { get => User.GetUserName(); }
+
+        /// <summary>
+        /// ユーザー
+        /// </summary>
+        private User User { get; }
+        #endregion
+
+        #region メソッド
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="user">ログインしたユーザー</param>
+        LoginUser(User user)
+        {
+            User = user;
+        }
+
+        /// <summary>
+        /// ログインしたユーザーを元にログインユーザーを生成する。
+        /// </summary>
+        /// <param name="user">ログインしたユーザー</param>
+        /// <returns>ログインユーザー</returns>
+        public static LoginUser CreateByUser(User user)
+            => new LoginUser(user);
+        #endregion
+    }
+}
---
+++ IssueManagementSystem/IssueManagementSystem.DomainModel/Auth/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueManagementSystem.DomainModel.Auth
+{
+    /// <summary>
+    /// ユーザー
+    /// </summary>
+    public class User 
+    {
+        #region プロパティ
+        /// <summary>
+        /// ユーザーID
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// ハッシュパスワード
+        /// </summary>
+        [Required]
+        public string HashPassword { get; set; }
+
+        /// <summary>
+        /// 名字
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// 名前
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+        #endregion
+
+        #region メソッド
+        /// <summary>
+        /// ユーザー名を取得する。
+        /// </summary>
+        /// <returns></returns>
+        public string GetUserName() => LastName + " " + FirstName;
+        #endregion
+    }
+}
