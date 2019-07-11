ed6661e4dcc61143618e97779f7e4bc807de843d
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-11 14:41:26 +0900

    IoCの準備。

---
+++ IssueManagementSystem/IssueManagementSystem.Persistence/IoC/ResolverConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Ninject;
+
+namespace IssueManagementSystem.Persistence.IoC
+{
+    /// <summary>
+    /// リゾルバー設定
+    /// </summary>
+    public class ResolverConfig
+    {
+        /// <summary>
+        /// リゾルバー設定
+        /// </summary>
+        public static void RegisterResolvers()
+        {
+            var kernel = new StandardKernel();
+
+            DependencyResolver.SetResolver(new MyDependencyResolver(kernel));
+        }
+    }
+}
