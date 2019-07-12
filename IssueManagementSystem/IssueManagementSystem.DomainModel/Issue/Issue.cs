5e910ff4a26687b38463534a7e99225d3f212265
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-12 16:10:07 +0900

    課題のドメインモデルを作成。

---
+++ IssueManagementSystem/IssueManagementSystem.DomainModel/Issue/Issue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueManagementSystem.DomainModel.Issue
+{
+    /// <summary>
+    /// 課題
+    /// </summary>
+    public class Issue
+    {
+        /// <summary>
+        /// 課題ID
+        /// </summary>
+        public int IssueId { get; set; }
+
+        /// <summary>
+        /// タイトル
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
