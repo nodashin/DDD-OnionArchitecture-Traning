0b4f08ebd73a6f542274d39996a688e4cdc358e6
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-12 09:24:01 +0900

    ログイン後メニューViewModel作成。

---
+++ IssueManagementSystem/IssueManagementSystem.ApplicationService.Web/ViewModels/Home/LoggedInMenuViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IssueManagementSystem.DomainModel.Auth;
+
+namespace IssueManagementSystem.ApplicationService.Web.ViewModels.Home
+{
+    /// <summary>
+    /// ログイン後メニューViewModel
+    /// </summary>
+    public class LoggedInMenuViewModel
+    {
+        #region プロパティ
+        /// <summary>
+        /// ログイン有無
+        /// </summary>
+        public bool LoggedIn { get => LoginUser != null; }
+
+        /// <summary>
+        /// ログインユーザーID
+        /// </summary>
+        public string LoginUserId { get => LoginUser?.UserId; }
+
+        /// <summary>
+        /// ログインユーザー名
+        /// </summary>
+        public string LoginUserName { get => LoginUser?.UserId; }
+
+        /// <summary>
+        /// ログインユーザー
+        /// </summary>
+        private LoginUser LoginUser { get; }
+        #endregion
+
+        #region メソッド
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="loginUser">ログインユーザー</param>
+        private LoggedInMenuViewModel(LoginUser loginUser)
+        {
+            LoginUser = loginUser;
+        }
+
+        /// <summary>
+        /// ログインユーザーからログイン後メニューViewModelを生成する。
+        /// </summary>
+        /// <param name="loginUser">ログインユーザー</param>
+        /// <returns>ログイン後メニューViewModel</returns>
+        public static LoggedInMenuViewModel CreateByLoginUser(LoginUser loginUser)
+            => new LoggedInMenuViewModel(loginUser);
+        #endregion
+    }
+}
