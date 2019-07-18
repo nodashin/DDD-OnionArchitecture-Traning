6dee21d2f1318df5c9b978b4f1e6bcc1dd24b861
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-18 09:47:01 +0900

    ユーザーDomainのテスト追加。

---
+++ IssueManagementSystem/IssueManagementSystem.DomainModelTests/Auth/UserTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IssueManagementSystem.DomainModel.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueManagementSystem.DomainModel.Auth.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        #region GetUserName
+        [TestMethod()]
+        public void GetUserNameTest()
+        {
+            var user = new User() { FirstName = "Lennon", LastName = "McCartney" };
+            Assert.AreEqual("McCartney Lennon", user.GetUserName());
+        }
+        #endregion
+    }
+}
