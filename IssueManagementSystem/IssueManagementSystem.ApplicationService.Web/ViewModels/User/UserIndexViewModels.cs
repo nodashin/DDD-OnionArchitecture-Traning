4243c8ccf78cba5b7b36c24db2c2e3332df6268b
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-18 09:12:23 +0900

    ユーザー一覧ViewModel作成。

---
+++ IssueManagementSystem/IssueManagementSystem.ApplicationService.Web/ViewModels/User/UserIndexViewModels.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IssueManagementSystem.ApplicationService.Web.ViewModels.Commons;
+
+namespace IssueManagementSystem.ApplicationService.Web.ViewModels.User
+{
+    #region ユーザー一覧ViewModel
+    public class UserIndexViewModel
+    {
+        #region プロパティ
+        /// <summary>
+        /// 検索条件
+        /// </summary>
+        public UserIndexSerchConditionViewModel SerchCondition { get; set; }
+
+        /// <summary>
+        /// ユーザー群
+        /// </summary>
+        /// <remarks>
+        /// データが0件でもnullにはならず、Countが0の状態となる。
+        /// </remarks>
+        public List<UserIndexUserViewModel> Users { get; set; }
+        #endregion
+    }
+    #endregion
+
+    #region ユーザー一覧 - 検索条件ViewModel
+    /// <summary>
+    /// ユーザー一覧 - 検索条件ViewModel
+    /// </summary>
+    public class UserIndexSerchConditionViewModel : SearchConditionViewModel
+    {
+        //TODO:後でなにか入れよう。
+    }
+    #endregion
+
+    #region ユーザー一覧 - ユーザーViewModel
+    /// <summary>
+    /// ユーザー一覧 - ユーザーViewModel
+    /// </summary>
+    public class UserIndexUserViewModel
+    {
+        #region プロパティ
+        /// <summary>
+        /// ユーザーID
+        /// </summary>
+        [DisplayName("ユーザーID")]
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        [DisplayName("ユーザー名")]
+        public string UserName { get; set; }
+        #endregion
+    }
+    #endregion
+}
---
+++ IssueManagementSystem/IssueManagementSystem.Web/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IssueManagementSystem.Web.Controllers
+{
+    /// <summary>
+    /// ユーザーController
+    /// </summary>
+    public class UserController : Controller
+    {
+        /// <summary>
+        /// ユーザー一覧Viewを表示する。
+        /// </summary>
+        /// <returns>ユーザー一覧View</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
